14a5f96b5800f8c8974f00c7ce88f1593bc95ca4
venkatesh18596@gmail.com <venkatesh18596@gmail.com>
2020-05-07 22:28:17 +0530

    Added new slide game. WIP

--- src/slide/index.js
+++ src/slide/index.js
@@ -5,22 +5,116 @@ class Slide extends Component {
 
   constructor(props) {
     super(props);
+    this.init = this.init.bind(this)
     this.slide = this.slide.bind(this)
+    this.changeWidth = this.changeWidth.bind(this)
+    this.changeHeight = this.changeHeight.bind(this)
+    this.updateSeq = this.updateSeq.bind(this)
     this.renderGrids = this.renderGrids.bind(this)
-    let state = this.init()
+    this.solve = this.solve.bind(this)
+    let state = this.init(false)
     this.state = state
   }
 
   // Trying to use init method for resetting the game
-  init() {
+  init(buttonClick) {
     let state = {
-      inp: [7, 2, 5, 1, 3, 8, 4, 6, 0]
+      inp: [0,7,2,1,8,5,4,3,6],
+      width: 3,
+      height: 3,
+      gridSeq: ""
+    }
+    if (buttonClick) {
+      state.width = parseInt(this.state.width)
+      state.height = parseInt(this.state.height)
+      state.inp = this.validateInp(this.state.gridSeq, state.width, state.height)
     }
     this.gridSize = 100
+    // Copy for the UI state
     this.input = state.inp.concat([]);
-    this.width = 3
-    this.height = 3
+    this.width = state.width
+    this.height = state.height
+    this.cache = {}
+    this.temp = 0
+    // not a best way to implement :(
+    if (buttonClick) {
+      this.setState(state)
+    }
     return state;
+  }
+
+  // Validating the input of the custom input sequence.
+  validateInp(inp, width, height) {
+    if (!inp) {
+      return this.generateSeq(width, height)
+    }
+    inp = inp.split(", ")
+    inp = inp.filter((value, index) => {
+      return inp.indexOf(value) === index
+    })
+    if (inp.length !== width*height) {
+      alert("Invalid input sequence")
+      return []
+    }
+    return inp.map((value) => {
+      return parseInt(value)
+    })
+  }
+
+  // TODO: Have to add real generate method based on
+  // https://www.cs.bham.ac.uk/~mdr/teaching/modules04/java2/TilesSolvability.html
+  generateSeq(width, height) {
+    return [3, 2, 6, 4, 7, 8, 13, 14, 11, 12, 9, 10, 1, 15, 5, 0]
+  }
+
+  // Solving the slide puzzle using BFS recurssion:)
+  // It shows solved but how to get the path ?
+  solve(inp) {
+    let nxtSeq = []
+    for (let j=0;j <inp.length; j++) {
+      let curSt = inp[j]
+      let possibleMoves = this.getPossibleMoves(curSt)
+      // TODO: change the below line
+      let res = "1,2,3,4,5,6,7,8,0"
+      for (let i=0; i<possibleMoves.length; i++) {
+        let curMov = possibleMoves[i]
+        let nxt = this.swap(curSt, curMov)
+        if (this.cache[nxt.join()]) {
+          continue
+        }
+        this.cache[nxt.join()] = true
+        nxtSeq.push(nxt)
+        if (nxt.join() === res) {
+          console.log(nxt);
+          alert("SOLVED")
+        }
+      }
+    }
+    return this.solve(nxtSeq)
+  }
+
+  // Swaps the value in the index to the zero placed index
+  // arr input does not mutate
+  swap(arr, index) {
+    let newArr = arr.concat([])
+    let indexEmp = newArr.indexOf(0)
+    newArr[indexEmp] = newArr[index]
+    newArr[index] = 0
+    return newArr;
+  }
+
+  // Return all the possible move (one tile) and return its indexes
+  getPossibleMoves(inp) {
+    let indexEmp = inp.indexOf(0)
+    // Getting all the four side index of the grid
+    let topIndex = indexEmp - this.width < 0 ? false : indexEmp - this.width
+    let bottomIndex = indexEmp + this.width >= this.width*this.height ? false : indexEmp + this.width
+    // Already the zero is in the left most cell, no further left positions available
+    let leftIndex = indexEmp % this.width === 0 ? false : indexEmp - 1
+    // The next right tile is below, so its not possible
+    let rightIndex = (indexEmp + 1) % this.width === 0 ? false : indexEmp + 1
+    // Returning only the non false values.
+    return [topIndex, bottomIndex, leftIndex, rightIndex].filter((value)=>{return value !== false})
   }
 
   // Sliding the clicked button to empty space if possible
@@ -37,11 +131,9 @@ class Slide extends Component {
 
     // Not possible move, The input grid is far away from empty grid
     let cellDiff = gridEmpX - gridInpX + gridEmpY - gridInpY
-    console.log(cellDiff)
     if (cellDiff !== 1 && cellDiff !== -1) {
       return
     }
-    console.log("pos")
     let prevX = parseInt(inp.target.style["left"])
     let prevY = parseInt(inp.target.style["top"])
     if (!prevX) {
@@ -50,17 +142,12 @@ class Slide extends Component {
     if (!prevY) {
       prevY = 0
     }
-    console.log(prevY, prevX, inp.target.style["left"])
     inp.target.style["left"] = prevX + ((gridEmpX - gridInpX) * 100)+"px";
     inp.target.style["top"] = prevY + ((gridEmpY - gridInpY) * 100)+"px";
     let tempState = {...this.state}
     tempState.inp[indexInp] = 0
     tempState.inp[indexEmp] = inpId
     this.setState(tempState)
-    console.log(tempState.inp)
-    console.log(gridInpX - gridEmpX)
-    console.log(indexInp, gridInpX, gridInpY)
-    console.log(indexEmp, gridEmpX, gridEmpY)
   }
 
   renderGrids(width, height, state) {
@@ -77,13 +164,53 @@ class Slide extends Component {
     return buttons
   }
 
+  changeWidth(inp) {
+    this.setState({width: parseInt(inp.target.value)})
+  }
+
+  changeHeight(inp) {
+    this.setState({height: parseInt(inp.target.value)})
+  }
+
+  updateSeq(inp) {
+    this.setState({gridSeq: inp.target.value})
+  }
+
+  renderInfo() {
+    return (
+      <div>
+        <label> Width :
+          <select onChange={this.changeWidth} value={this.state.width}>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <label> Height :
+          <select onChange={this.changeHeight} value={this.state.height}>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        </label>
+        <br></br>
+        Grid Sequence : <input type="text" onChange={this.updateSeq} value={this.state.gridSeq} placeholder="Comma seperated input" />
+        <br></br>
+        <input type="button" value="New Game" onClick={() => this.init(true)} />
+      </div>
+    )
+  }
+
   render() {
     return (
       <div className={styles.container}>
+        <div className={styles.infoContainer}>
+          {this.renderInfo()}
+        </div>
         <div className={styles.buttonContainer}>
           {this.renderGrids(this.width, this.height, this.state)}
         </div>
-        <div className={styles.infoContainer}></div>
+        <input type="button" value="Solve" onClick = {() => this.solve([this.state.inp])} />
       </div>
     )
   }
